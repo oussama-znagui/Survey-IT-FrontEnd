6e21e18825e071ffd90c582f7b5f74075d7161f7
znagui <znagui.info@gmail.com>
2024-12-26 09:55:58 +0100

    Creation du dashboard pour gestion des editions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SurveysListComponent } from './features/dashboard/components/surveys-list/surveys-list.component';
 import { DashboardComponent } from './features/dashboard/dashboard/dashboard.component';
 import { MainPageComponent } from './features/dashboard/main-page/main-page.component';
+import { EditionDetailsComponent } from './features/dashboard/components/edition-details/edition-details.component';
 
 
 export const routes: Routes = [
     {path:'' , component:MainPageComponent},
-    {path: 'surveys-list', component: SurveysListComponent}
+    {path: 'surveys-list', component: SurveysListComponent},
+    {path: 'edition',component:  EditionDetailsComponent}
 ];
---
+++ src/app/features/dashboard/components/edition-details/edition-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edition-details',
+  imports: [],
+  templateUrl: './edition-details.component.html',
+  styleUrl: './edition-details.component.css'
+})
+export class EditionDetailsComponent {
+
+}
