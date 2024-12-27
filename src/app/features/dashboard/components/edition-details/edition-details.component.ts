eabf914733d318bce1880a5a447609c9003cb6bc
znagui <znagui.info@gmail.com>
2024-12-27 16:10:39 +0100

    gestion des editions et affichege des sujets et questions(Routing)

--- src/app/features/dashboard/components/edition-details/edition-details.component.ts
+++ src/app/features/dashboard/components/edition-details/edition-details.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { SubjectsTreeComponent } from '../subjects-tree/subjects-tree.component';
 import { QuestionsListComponent } from '../questions-list/questions-list.component';
 import { ActivatedRoute } from '@angular/router';
+import { EditionService } from '../../services/edition.service';
+import { Edition } from '../../models/edition';
+import { error } from 'console';
 
 @Component({
   selector: 'app-edition-details',
@@ -11,16 +14,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditionDetailsComponent {
   id!: number;
+  edition!: Edition
 
-constructor(private route: ActivatedRoute){
+constructor(private route: ActivatedRoute,private service: EditionService){
 
 }
 
 ngOnInit(){
   this.route.params.subscribe(params => {
     this.id = +params['id'];
+    this.getEditionData(this.id);
   })
 
+  
 }
+
+getEditionData(id: number){
+  this.service.getEditionDate(id).subscribe(
+    data => {this.edition = data;},
+  
+  error => {
+    console.log(error);
+  })
+}
+
 
 }
